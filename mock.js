553cf7586f607ddd052f7db09aa347a9ec833b19
Ed_zg <838298008@qq.com>
2017-01-05 18:23:57 +0800

    新闻首页修改

--- mock.js
+++ mock.js
@@ -1,39 +1,56 @@
 module.exports = {
-  "articalList":[
+  "articleList": [
     {
-      "avator":{
-        "name":"张三1",
-        "img":"http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"
+      "id": 0,
+      "avator": {
+        "name": "张三1",
+        "img": "/images/avator/0.jpg",
+        "time": (new Date().getHours())+':'+(new Date().getMinutes())+':'+(new Date().getSeconds())+':'+(new Date().getMilliseconds())
       },
-      "abstractImg":"http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg",
-      "title":"印度神话专题-湿婆的冷漠",
-      "abstract":"印度神话专题-湿婆的冷漠的简介打发撒打算发印度神话专题-湿婆的冷漠的简介打发撒打算发印度神话专题-湿婆的冷漠的简介打发撒打算发"
+      "abstractImg": "/images/newsImg/0.jpg",
+      "title": "印度神话专题-湿婆的冷漠",
+      "abstract": "张三1印度神话专题-湿婆的冷漠的简介打发撒打算发印度神话专题-湿婆的冷漠的简介打发撒打算发印度神话专题-湿婆的冷漠的简介打发撒打算发",
+      "remarks": "11",
+      "collections": "11"
     },
     {
-      "avator":{
-        "name":"李四",
-        "img":"http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"
+      "id": 1,
+      "avator": {
+        "name": "李四",
+        "img": "/images/avator/1.jpg",
+        "time": (new Date().getHours())+':'+(new Date().getMinutes())+':'+(new Date().getSeconds())+':'+(new Date().getMilliseconds())
       },
-      "title":"印度神话专题-湿婆的冷漠",
-      "abstract":"印度神话专题-湿婆的冷漠的简介打发撒打算发印度神话专题-湿婆的冷漠的简介打发撒打算发印度神话专题-湿婆的冷漠的简介打发撒打算发"
+      "title": "李四的文章-湿婆的冷漠",
+      "abstractImg": "/images/newsImg/1.jpg",
+      "abstract": "李四印度神话专题-湿婆的冷漠的简介打发撒打算发印度神话专题-湿婆的冷漠的简介打发撒打算发印度神话专题-湿婆的冷漠的简介打发撒打算发",
+      "remarks": "0",
+      "collections": "22"
     },
     {
-      "avator":{
-        "name":"赵武",
-        "img":"http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"
+      "id": 2,
+      "avator": {
+        "name": "赵武",
+        "img": "/images/avator/2.jpg",
+        "time": (new Date().getHours())+':'+(new Date().getMinutes())+':'+(new Date().getSeconds())+':'+(new Date().getMilliseconds())
       },
-      "abstractImg":"http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg",
-      "title":"12印度神话专题-湿婆的冷漠",
-      "abstract":"印度神话专题-湿婆的冷漠的简介打发撒打算发印度神话专题-湿婆的冷漠的简介打发撒打算发印度神话专题-湿婆的冷漠的简介打发撒打算发"
+      "abstractImg": "/images/newsImg/2.jpg",
+      "title": "赵武-湿婆的冷漠",
+      "abstract": "赵武印度神话专题-湿婆的冷漠的简介打发撒打算发印度神话专题-湿婆的冷漠的简介打发撒打算发印度神话专题-湿婆的冷漠的简介打发撒打算发",
+      "remarks": "33",
+      "collections": "0"
     },
     {
-      "avator":{
-        "name":"老六",
-        "img":"http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"
+      "id": 3,
+      "avator": {
+        "name": "老六",
+        "img": "/images/avator/3.jpg",
+        "time": (new Date().getHours())+':'+(new Date().getMinutes())+':'+(new Date().getSeconds())+':'+(new Date().getMilliseconds())
       },
-      "abstractImg":"http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg",
-      "title":"12印度神话专题-湿婆的冷漠",
-      "abstract":"印度神话专题-湿婆的冷漠的简介打发撒打算发印度神话专题-湿婆的冷漠的简介打发撒打算发印度神话专题-湿婆的冷漠的简介打发撒打算发"
+      "abstractImg": "/images/newsImg/3.jpg",
+      "title": "老六12印度神话专题-湿婆的冷漠",
+      "abstract": "老六印度神话专题-湿婆的冷漠的简介打发撒打算发印度神话专题-湿婆的冷漠的简介打发撒打算发印度神话专题-湿婆的冷漠的简介打发撒打算发",
+      "remarks": "44",
+      "collections": "44"
     }
   ]
 };
--- pages/index/index.js
+++ pages/index/index.js
@@ -2,7 +2,7 @@
 //获取应用实例
 let mock = require('../../mock.js');
 console.log(mock);
-var app = getApp();
+let app = getApp();
 Page({
   data: {
     hotBtn: [
@@ -11,15 +11,15 @@ Page({
       '分享'
     ],
     userInfo: {},
-    articalList: mock.articalList,
+    articleList: mock.articleList,
     imgUrls: [
         '/images/swiper/0.jpg',
         '/images/swiper/1.jpg',
         '/images/swiper/2.jpg'
     ]
   },
-  onLoad: function () {
-    var that = this;
+  onLoad() {
+    console.log('onload完成');
     //调用应用实例的方法获取全局数据
     // wx.request({
     //   url:'mock',
@@ -30,5 +30,30 @@ Page({
     //     consle.log('0');
     //   }
     // });
+  },
+  onUnload() {
+    console.log('unload');
+  },
+  onHide() {
+    console.log('hide')
+  },
+  onReady() {
+    console.log('页面onload+onshow后触发onReady');
+  },
+  onShow() {
+    console.log('onload后触发onShow');
+  },
+  onShareAppMessage() {
+    return {
+      title: 'Edward知乎', 
+      desc: 'Edward的知乎内容...', 
+      path: 'path'
+    };
+  },
+  onPullDownRefresh() {
+    console.log('向下拉获取新数据事件')
+  },
+  onReachBottom() {
+    console.log('向下拉到底部事件');
   }
 })
